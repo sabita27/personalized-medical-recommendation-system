27bdedf2126bd156d5eb2e2d7a2d7efd67be64fe
Sabita Sau <sausabita62@gmail.com>
2026-05-05 17:03:20 +0530

    Remove resume_builder project

--- resume_builder/routes/web.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ResumeController;
-
-Route::get('/', [ResumeController::class, 'index']);
-Route::get('/Sabita_Sau_Resume_2026.pdf', [ResumeController::class, 'download']);
